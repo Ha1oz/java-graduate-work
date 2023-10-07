189d42f924beac9737a652a9d1375cdd488623aa
d1mk0v <79874763588@ya.ru>
2023-10-07 21:28:12 +0500

    some fix's

--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateAdDto.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAdDto.java
@@ -3,7 +3,7 @@ package ru.skypro.homework.dto;
 import lombok.Data;
 
 @Data
-public class CreateOrUpdateAdDTO {
+public class CreateOrUpdateAdDto {
     private String description;
     private Long price;
     private String title;
